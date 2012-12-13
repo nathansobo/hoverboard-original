2a1f67ed322ceeaf9cffbba2c709970df2bb2a50
Nathan Sobo <nathan@github.com>
2012-12-13 13:08:47 -0800

    Upgrade event-tap

--- node_modules/event-tap/event-tap.js
+++ node_modules/event-tap/event-tap.js
@@ -1,3 +1,4 @@
 eventTap = require("bindings")("event_tap");
 exports.postKeyboardEvent = eventTap.postKeyboardEvent;
 exports.postMouseEvent = eventTap.postMouseEvent;
+exports.getMouseLocation = eventTap.getMouseLocation;
--- node_modules/event-tap/src/event_tap.mm
+++ node_modules/event-tap/src/event_tap.mm
@@ -44,9 +44,27 @@ Handle<Value> PostMouseEvent(const Arguments& args) {
   return scope.Close(Undefined());
 }
 
+Handle<Value> GetMouseLocation(const Arguments& args) {
+  HandleScope scope;
+  CGEventRef  event;
+  CGPoint point;
+  Local<Object> coordinates;
+
+  coordinates = Object::New();
+  event = CGEventCreate(NULL);
+  point = CGEventGetLocation(event);
+
+  coordinates->Set(String::NewSymbol("x"), Integer::New(point.x));
+  coordinates->Set(String::NewSymbol("y"), Integer::New(point.y));
+
+  CFRelease(event);
+  return scope.Close(coordinates);
+}
+
 void Init(Handle<Object> target) {
   target->Set(String::NewSymbol("postKeyboardEvent"), FunctionTemplate::New(PostKeyboardEvent)->GetFunction());
   target->Set(String::NewSymbol("postMouseEvent"), FunctionTemplate::New(PostMouseEvent)->GetFunction());
+  target->Set(String::NewSymbol("getMouseLocation"), FunctionTemplate::New(GetMouseLocation)->GetFunction());
 }
 
 NODE_MODULE(event_tap, Init)
